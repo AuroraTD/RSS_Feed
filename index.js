378e11cec09e3d77a036602972cd0f84f7a9a87e
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-06-12 10:56:32 -0400

    Read feed links from file

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 // REQUIRE
 const Parser = require('rss-parser');
+const readline = require('readline');
+const fs = require('fs');
 
 // CONSTANTS
 const oParser = new Parser();
-const N_FEED_SIZE = 20;
 
-// TODO read feeds from file
 // TODO publish resultant feed
 // TODO publish X articles per day
-// TODO add file level comments
+// TODO add file level comments (see rest API in portfolio)
 // TODO fix jshint complaints
 
+// VARIABLES
+let oReadLine;
+let aFeedLinks;
+let aPromises;
+let aFeeds;
+
+// GET FEEDS
+aFeedLinks = [];
+oReadLine = readline.createInterface({
+    input: fs.createReadStream('feeds.txt')
+});
+oReadLine.on('line', function(sLine){
+    aFeedLinks.push(sLine);
+});
+oReadLine.on('close', function(sLine){
+    parseFeeds(aFeedLinks);
+});
+
 // PARSE
-(async function () {
-    let oFeed = await oParser.parseURL('http://feeds.bbci.co.uk/news/technology/rss.xml');
-    console.log(oFeed.title);
-    oFeed.items.forEach(function(oItem){
-        console.log(`${oItem.title} : ${oItem.link}`);
+async function parseFeeds(aFeedLinks) {
+    aPromises = aFeedLinks.map(function(sLink){
+        return oParser.parseURL(sLink);
     });
-})();
+    aFeeds = await Promise.all(aPromises);
+    aFeeds.forEach(function(oFeed){
+        console.log(oFeed.title);
+        oFeed.items.forEach(function(oItem){
+            console.log(`${oItem.title} : ${oItem.link}`);
+        });
+    });
+}

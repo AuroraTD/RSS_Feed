e69e9ee1ddaca03f0dc0607de49315b6433c30fd
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-06-12 11:46:37 -0400

    Randomly select news items

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const fs = require('fs');
 
 // CONSTANTS
 const oParser = new Parser();
+const N_DESIRED_ITEMS = 10;
 
 // TODO publish resultant feed
-// TODO publish X articles per day
+// TODO fix egregious delays as number desired items increases
 // TODO add file level comments (see rest API in portfolio)
 // TODO fix jshint complaints
+// TODO add error handling
 
 // VARIABLES
 let oReadLine;
@@ -31,14 +33,36 @@ oReadLine.on('close', function(sLine){
 
 // PARSE
 async function parseFeeds(aFeedLinks) {
+    let aItemsAll = [];
+    let aItemsSingleFeed;
+    let i;
     aPromises = aFeedLinks.map(function(sLink){
         return oParser.parseURL(sLink);
     });
     aFeeds = await Promise.all(aPromises);
     aFeeds.forEach(function(oFeed){
-        console.log(oFeed.title);
-        oFeed.items.forEach(function(oItem){
-            console.log(`${oItem.title} : ${oItem.link}`);
-        });
+        aItemsSingleFeed = randomlySelectItems(oFeed.items);
+        aItemsAll = aItemsAll.concat(aItemsSingleFeed);
     });
+    aItemsAll = randomlySelectItems(aItemsAll);
+    for (i = 0; i < N_DESIRED_ITEMS; i++) {
+        console.log(`${aItemsAll[i].title} : ${aItemsAll[i].link}`);
+    }
+}
+
+// RANDOMLY SELECT
+function randomlySelectItems (aItemsIN) {
+
+    let oIndices = new Set();
+    let aItemsOUT = [];
+
+    while (aItemsOUT.length < N_DESIRED_ITEMS) {
+        let nRandom = Math.floor(Math.random() * aItemsIN.length);
+        if (oIndices.has(nRandom) === false) {
+            oIndices.add(nRandom);
+            aItemsOUT.push(aItemsIN[nRandom]);
+        }
+    }
+
+    return aItemsOUT;
 }
